73b9d2c2c2cba295fcbfccaff08e938a5bd1c7dc
johnarclear-cloud <johnarclear@gmail.com>
2026-02-25 23:39:38 +0000

    Update script1.py

--- script1.py
+++ script1.py
@@ -3,7 +3,7 @@ from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
 import sys 
 
-nr = InitNornir(config_file="prod_config")
+nr = InitNornir(config_file="prod_config.yaml")
 
 def test_connect(task):
     task.run(task=netmiko_send_command, command_string="show configuration")
